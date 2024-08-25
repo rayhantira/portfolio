82c0159ec856e956e29be94d90b3209d5eb09390
Rayhan Yulistira <ryulistira@gmail.com>
2024-08-25 17:16:36 +0700

    UI Update

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#76ABAE",
+        lprimary: "#677791",
+        dark: "#0000",
+        body: "#ffffff",
+        dbody: "#ffffee",
+        subhead: "#8b8b8b",
+        lsubhead: "#929292",
+        cards: "#202022",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        hanken: ["hanken", "sans-serif"],
+      },
+      screens: {
+        sm: "414px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
+  },
+  plugins: [
+    // ...
+    require("tailwind-scrollbar"),
+  ],
+};
